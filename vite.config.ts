2dc25a12177a34aeaaa77e95029b6f0201c772b1
Chiara <chiara-zuc@libero.it>
2025-01-23 21:59:37 +0100

    fix --> added vite.config for paths

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from "vite";
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+export default defineConfig({
+  resolve: {
+    alias: {
+      "@enums": path.resolve(__dirname, "src/enums"),
+      "@scenes": path.resolve(__dirname, "src/scenes"),
+    },
+  },
+});
