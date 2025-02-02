d5b8582d116a50cd349f4305f653d7da7926abae
Chiara <chiara-zuc@libero.it>
2025-02-02 17:05:27 +0100

    vite config

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,9 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
+  worker: {
+    format: "es"
+  },
   resolve: {
     alias: {
       "@enums": path.resolve(__dirname, "src/enums"),
